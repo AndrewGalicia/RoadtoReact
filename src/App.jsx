8ea7bf6b5e14ad02efa61b3e268100e334590036
Andrew Galicia <andrewlgalicia@gmail.com>
2024-11-12 15:03:00 -0800

    lesson 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,53 @@
 import * as React from 'react';
 
-const todoList = [
-  {id: 1, title: "buy milk"},
-  {id: 2, title: "wash car"},
-  {id: 3, title: "walk dog"},
-  
-]
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
-function App() {
-  
-  return (
-    
-    <div>
-      <h1>ToDo List</h1>
-      <ul>
-        {todoList.map(todoList => (
-          <li key={todoList.id}>{todoList.id}</li>,
-          <li>{todoList.title}</li>
-        ) )}
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
+    <Search />
+    <hr />
+    <List />
+  </div>
+);
 
+const List = () => (
+  <ul>
+    {list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
+    ))}
+  </ul>
+);
 
-      </ul>
-  
-    </div> );
-      }
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search:</label>
+    <input id="search" type="text" />
+  </div>
+);
 
 export default App;
